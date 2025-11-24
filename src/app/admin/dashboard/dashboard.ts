df5a26b064a23630a96d93f8f76a57fee1fa9f17
MartinPPY <martinsantiago.se@gmail.com>
2025-11-23 21:05:30 -0300

    feat: preparando para integrar backend

--- src/app/admin/dashboard/dashboard.ts
+++ src/app/admin/dashboard/dashboard.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { GeneralModule } from '../../shared/general/general-module';
 import { MaterialModuleModule } from '../../shared/material-module/material-module-module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [GeneralModule,MaterialModuleModule],
+  imports: [GeneralModule, MaterialModuleModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
+
+  private _fb: FormBuilder = inject(FormBuilder)
+
+
+  emergencyForm: FormGroup = this._fb.group({
+    description: ['', [Validators.required]],
+    victims: [0, [Validators.required]],
+    severity: ['', [Validators.required]]
+  })
+
+  medics = []
+  emergencies = []
+
+  registerEmergency(){
+
+    console.log(this.emergencyForm.value)
+  }
+
+
 
 }
