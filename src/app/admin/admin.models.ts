9e07788751e2e5d84864dc26c5d3e32057938731
MartinPPY <martinsantiago.se@gmail.com>
2025-11-27 13:15:14 -0300

    feat: fixed admin view

--- src/app/admin/admin.models.ts
+++ src/app/admin/admin.models.ts
@@ -1,7 +1,15 @@
-export interface Medic{
-    availability:boolean,
-    email:string,
-    lastname:string,
-    name:string,
-    phone:number
+export interface Medic {
+    id?:number
+    availability: boolean,
+    email: string,
+    lastname: string,
+    name: string,
+    phone: number
+}
+
+export interface Emergency{
+    description:string,
+    victims:number,
+    severity:string,
+    medics:number[]
 }
--- src/app/admin/dashboard/components/form/form.ts
+++ src/app/admin/dashboard/components/form/form.ts
@@ -7,6 +7,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PageEvent } from '@angular/material/paginator';
 import { SelectionChange } from '@angular/cdk/collections';
 import { MatSelectionListChange } from '@angular/material/list';
+import { Emergency } from '../../../admin.models';
 
 @Component({
   selector: 'app-form',
@@ -19,7 +20,7 @@ export class Form implements OnInit {
   adminService = inject(Admin)
   private _alertService = inject(Alert)
   private _fb: FormBuilder = inject(FormBuilder)
-  selectedMedics:number[] = []
+  selectedMedics: number[] = []
 
   pageIndexMedicsForm = signal(0)
   pageSizeMedicsForm = signal(5)
@@ -50,7 +51,37 @@ export class Form implements OnInit {
   }
 
   registerEmergency() {
-    console.log(this.emergencyForm.value)
+
+    if (this.emergencyForm.get('description')?.hasError('required')) {
+      this._alertService.alert('Error', 'VERIFICA LA DESCRIPCION')
+      return
+    }
+
+    if (this.emergencyForm.get('victims')?.hasError('required')) {
+      this._alertService.alert('Error', 'VERIFICA LA CANTIDAD DE VICTIMAS')
+      return
+    }
+
+    if (this.emergencyForm.get('severity')?.hasError('required')) {
+      this._alertService.alert('Error', 'VERIFICA LA SEVERIDAD DE LA EMERGENCIA')
+      return
+    }
+
+    if(this.selectedMedics.length == 0){
+      this._alertService.alert('Error','NO HAS SELECCIONADO MEDICOS!')
+      return
+    }
+
+    const emergency: Emergency = {
+      description: this.emergencyForm.get('description')?.value,
+      medics: this.selectedMedics,
+      severity: this.emergencyForm.get('severity')?.value,
+      victims: this.emergencyForm.get('victims')?.value
+    }
+
+    console.log(emergency)
+
+
   }
 
   onPageMedicForm(event: PageEvent) {
@@ -61,7 +92,7 @@ export class Form implements OnInit {
 
   onSelectionChange(event: MatSelectionListChange) {
     const selectedMedics = event.source.selectedOptions.selected.map(op => op.value)
-    console.log(selectedMedics)
+    this.selectedMedics = selectedMedics
 
   }
 
